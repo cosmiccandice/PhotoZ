0f42dddba7dd0d40e79d14293262afecdaa6b42c
Candice Stauffer <56940363+cosmiccandice@users.noreply.github.com>
2022-09-29 08:02:20 -0500

    creating sqlquery to get sdss table and save as csv file

---
+++ sdss_query.py
@@ -0,0 +1,27 @@
+from astroquery.sdss import SDSS
+import sqlite3 as db
+import csv
+
+
+sdss_query = """
+SELECT
+z,zerr,zwarning,class,subclass,survey,type,objid,specobjid, ra, dec
+FROM SpecPhoto 
+WHERE 
+ (class = 'QSO') OR (class= 'GALAXY')
+""" 
+
+#save data
+data = SDSS.query_sql(sdss_query)
+
+
+#create csv
+table_name = 'sdss_table'
+with open(table_name, 'w', newline='') as f_handle:
+    writer = csv.writer(f_handle)
+    # Add the header/column names
+    header = ['z','zerr','zwarning','class','subclass','subclass','survey,','type','objid','specobjid','ra','dec']
+    writer.writerow(header)
+    # Iterate over `data`  and  write to the csv file
+    for row in data:
+        writer.writerow(row)
