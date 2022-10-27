c9b1813f45882a44a80193b6ab09fa9fd0f3b73f
Candice Stauffer <56940363+cosmiccandice@users.noreply.github.com>
2022-10-27 14:47:27 -0500

    sdss_query bug

--- sdss_query.py
+++ sdss_query.py
@@ -4,7 +4,7 @@ import csv
 
 
 sdss_query = """
-SELECT *
+SELECT
 z,zerr,zwarning,class,subclass,survey,type,objid,specobjid, ra, dec
 FROM SpecPhoto 
 WHERE 
@@ -17,10 +17,11 @@ data = SDSS.query_sql(sdss_query)
 
 #create csv
 table_name = 'sdss_table.csv'
+
 with open(table_name, 'w', newline='') as f_handle:
     writer = csv.writer(f_handle)
     # Add the header/column names
-    header = ['z','zerr','zwarning','class','subclass','survey,','type','objid','specobjid','raMean','decMean']
+    header = ['z','zerr','zwarning','class','subclass','survey,','type','objid','specobjid','ra','dec']
     writer.writerow(header)
     # Iterate over `data`  and  write to the csv file
     for row in data:
