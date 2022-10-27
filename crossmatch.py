066faf775bb2bc0bd68ca9cf206d45d7df9b5dae
Candice Stauffer <56940363+cosmiccandice@users.noreply.github.com>
2022-10-27 15:41:50 -0500

    crossmatch bug

--- crossmatch.py
+++ crossmatch.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-sdss  = pd.read_csv("sdss_table.csv")
+sdss  = pd.read_csv("../1_catalogs/sdss_table.csv")
 print(sdss[0:20]) 
 
 psdr2 =pd.read_csv("../1_catalogs/panSTARRS/db_data/SOV_05.00.db.csv") 
