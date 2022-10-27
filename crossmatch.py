a5fea3600c0dd3f294a9a1cac19ad56d5e251ff9
Candice Stauffer <56940363+cosmiccandice@users.noreply.github.com>
2022-10-27 17:44:33 -0500

    adding file to split up large .db.csv files

--- crossmatch.py
+++ crossmatch.py
@@ -1,8 +1,3 @@
 import pandas as pd
-
-
 sdss  = pd.read_csv("../1_catalogs/sdss_table.csv")
-print(sdss[0:20]) 
 
-psdr2 =pd.read_csv("../1_catalogs/panSTARRS/db_data/SOV_05.00.db.csv") 
-print (psdr2[0:20])
